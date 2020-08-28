69816389e0b4f0a6a7fbf4c577a45ffa890ca782
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2020-08-28 20:10:52 -0300

    Reading file

--- yalul/cli/main.py
+++ yalul/cli/main.py
@@ -1,11 +1,16 @@
 import click
 from yalul.source import Source
 
-
 @click.command()
-def execute():
-    click.echo(
-        'Welcome to yalul, acronym for "Yet another language to understand languages"\n\n'
-        'yalul is a simple tree-walk interpreter written in python for easy understanding and study of programming '
-        'languages, compilers and interpreters. '
-    )
+@click.argument('filename', required=False)
+def execute(filename):
+    if filename is None:
+        click.echo(
+            'Welcome to yalul, acronym for "Yet another language to understand languages"\n\n'
+            'yalul is a simple tree-walk interpreter written in python for easy understanding and study of programming '
+            'languages, compilers and interpreters. '
+        )
+    else:
+        source_file = open(filename, 'r')
+        source = Source(source_file)
+        click.echo(source.read())
