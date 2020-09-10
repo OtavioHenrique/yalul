5c8537733459da229862b7ab9d69f157ab64e0ec
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2020-09-10 19:31:44 -0300

    Lexer docs

--- yalul/lexer.py
+++ yalul/lexer.py
@@ -4,16 +4,30 @@ from yalul.lex.token_type import TokenType
 
 
 class Lexer:
+    """
+    Yalul's own Lexer.
+
+    This class has as it main objective receive a source code, scan it and produce a list of language tokens.
+    """
     def __init__(self, source):
+        """
+        Construct a new Lexer object.
+
+        :param source: A opened file
+        :return: returns nothing
+        """
         self.source = source
         self.file_size = os.fstat(source.fileno()).st_size
 
     def run(self):
+        """
+        Return a list of tokens for the source
+        """
         current_char = self.source.read(1)
 
         tokens_list = []
 
-        while not self.__end_of_file(current_char):
+        while not self.__end_of_file():
             if current_char == ' ' or current_char == '\n':
                 current_char = self.source.read(1)
 
@@ -34,5 +48,5 @@ class Lexer:
     def __is_digit(self, char):
         return '0' <= char <= '9'
 
-    def __end_of_file(self, char):
+    def __end_of_file(self):
         return self.source.tell() >= self.file_size
