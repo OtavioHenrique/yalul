4da7e35479e15b22009c41618c596c18729a1e32
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2020-11-14 23:33:29 -0300

    add complex ast specs to operations

--- tests/parser/binary_operation/parse_binary_multiply_test.py
+++ tests/parser/binary_operation/parse_binary_multiply_test.py
@@ -32,3 +32,35 @@ class TestParseBinaryMultiply:
 
         assert ast.right.value == 1
         assert type(ast.right) == Value
+
+    def test_parser_run_generates_correct_ast_complex_binary_expression_multiply(self):
+        """
+        Validates if parser is generating a correct AST to a complex binary operation sum, like 39 * 1 * 2
+        """
+        tokens = [
+            Token(TokenType.INTEGER, 39),
+            Token(TokenType.MULTIPLY, "*"),
+            Token(TokenType.INTEGER, 1),
+            Token(TokenType.MULTIPLY, "*"),
+            Token(TokenType.INTEGER, 2),
+            Token(TokenType.EOF, "End of File")
+        ]
+
+        ast = Parser(tokens).parse()
+
+        ast = ast[0]
+
+        assert type(ast) is Binary
+        assert ast.operator.type is TokenType.MULTIPLY
+
+        assert type(ast.left) is Value
+        assert ast.left.value == 39
+
+        assert type(ast.right) is Binary
+        assert ast.right.operator.type is TokenType.MULTIPLY
+
+        assert type(ast.right.left) is Value
+        assert ast.right.left.value == 1
+
+        assert type(ast.right.right) is Value
+        assert ast.right.right.value == 2
