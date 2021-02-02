287c45cf4e539f6194ab16cfaeaa921fdef39a3c
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2021-02-02 20:03:07 -0300

    ref: refactor lexer to work as expected

--- yalul/lexer.py
+++ yalul/lexer.py
@@ -35,38 +35,35 @@ class Lexer:
 
         tokens_list = []
 
-        while not self.__end_of_file():
+        is_at_end = False
+
+        while not is_at_end:
             if current_char == ' ':
                 current_char = self.source.read(1)
-
-            if current_char == '\n':
+            elif current_char == '\n':
                 if tokens_list[-1].type != TokenType.END_STATEMENT:
-                    tokens_list.append(Token(TokenType.END_STATEMENT, "End of Staatement"))
+                    tokens_list.append(Token(TokenType.END_STATEMENT, "End of Statement"))
 
                 current_char = self.source.read(1)
-
-            if current_char == '"':
+            elif current_char == '"':
                 string = StringScanner(current_char, self.source).create_token()
 
                 tokens_list.append(string)
 
                 current_char = self.source.read(1)
-
-            if NumbersScanner.is_digit(current_char):
+            elif NumbersScanner.is_digit(current_char):
                 scanner = NumbersScanner(current_char, self.source)
 
                 tokens_list.append(scanner.create_token())
 
                 current_char = scanner.current_char
-
-            if OperatorScanner.is_operator(current_char):
+            elif OperatorScanner.is_operator(current_char):
                 token = OperatorScanner(current_char).create_token()
 
                 tokens_list.append(token)
 
                 current_char = self.source.read(1)
-
-            if ComparisonOperatorScanner.is_comparison(current_char):
+            elif ComparisonOperatorScanner.is_comparison(current_char):
                 two_digit_token_list = [
                     TokenType.LESS_EQUAL,
                     TokenType.GREATER_EQUAL,
@@ -80,27 +77,32 @@ class Lexer:
                 tokens_list.append(token)
 
                 current_char = scanner.current_char
-
-            if GroupingScanner.is_paren(current_char):
+            elif GroupingScanner.is_paren(current_char):
                 scanner = GroupingScanner(current_char, self.source)
                 token = scanner.create_token()
 
                 tokens_list.append(token)
 
                 current_char = scanner.current_char
-
-            if KeywordScanner.is_alpha(current_char):
+            elif KeywordScanner.is_alpha(current_char):
                 scanner = ComparisonOperatorScanner(current_char, self.source)
                 token = scanner.create_token()
 
                 tokens_list.append(token)
 
                 current_char = scanner.current_char
+            elif current_char == '':
+                if self.source.tell() == self.file_size:
+                    is_at_end = True
+                else:
+                    self.source.read(1)
 
-        tokens_list.append(Token(TokenType.END_STATEMENT, "End of Staatement"))
+        if tokens_list[-1].type != TokenType.END_STATEMENT:
+            tokens_list.append(Token(TokenType.END_STATEMENT, "End of Statement"))
+
         tokens_list.append(Token(TokenType.EOF, "End of File"))
 
         return tokens_list
 
     def __end_of_file(self):
-        return self.source.tell() >= self.file_size
+        return self.source.tell() > self.file_size
