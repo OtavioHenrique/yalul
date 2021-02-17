4ffa62458284247afd00b5e83fb85a3874451592
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2021-02-17 19:56:20 -0300

    min refactors

--- yalul/parsers/ast/nodes/statements/expression.py
+++ yalul/parsers/ast/nodes/statements/expression.py
@@ -1,3 +1,12 @@
 class Expression:
+    """
+    Yalul's expression statement
+    """
+
     def __init__(self, value):
+        """
+        Construct a new expression statement object.
+
+        :name: expression value
+        """
         self.value = value
