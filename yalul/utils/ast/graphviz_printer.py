5625878a3acd4aa705b743511a2d24c303f364f1
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2021-03-03 20:18:40 -0300

    Render blocks

--- yalul/utils/ast/graphviz_printer.py
+++ yalul/utils/ast/graphviz_printer.py
@@ -1,5 +1,6 @@
 import uuid
 
+from yalul.parsers.ast.nodes.statements.block import Block
 from yalul.parsers.ast.nodes.statements.expressions.binary import Binary
 from yalul.parsers.ast.nodes.statements.expressions.grouping import Grouping
 from yalul.parsers.ast.nodes.statements.expressions.variable import Variable
@@ -27,22 +28,50 @@ class GraphvizPrinter:
         self.ast = ast
 
     def generate_pdf(self):
-        dot = Digraph(comment='AST', node_attr={'shape': 'record', 'height': '.1'})
+        graph = Digraph(comment='AST', node_attr={'shape': 'record', 'height': '.1'})
 
         for statement in self.ast:
-            if type(statement) == VariableDeclaration:
-                dot.node('VariableDeclaration', '<f0> Name|<f1> Variable Declaration|<f2> Value')
-                self.__render_expression(dot, statement.initializer, 'VariableDeclaration:f2')
-                dot.node('Identifier', '<f0> Identifier |<f2> {}'.format(statement.name))
-                dot.edge('VariableDeclaration:f0', 'Identifier:f0')
-            if isinstance(statement, Expression):
-                self.__render_expression(dot, statement)
+            self.__render_statement(graph, statement)
 
-        dot.render('test-output/round-table.gv', view=True)
+        graph.render('test-output/round-table.gv', view=True)
 
-    def __render_expression(self, graph, expression, previous_node=None, count='0'):
+    def __render_statement(self, graph, statement, previous_node=None, identifier=None):
+        if type(statement) == VariableDeclaration:
+            self.render_var_declaration_statement(graph, statement, previous_node, identifier)
+        if type(statement) == Block:
+            self.render_block_statement(graph, identifier, previous_node, statement)
+        if isinstance(statement, Expression):
+            self.__render_expression(graph, statement, previous_node, statement)
+
+    def render_var_declaration_statement(self, graph, statement, previous_node=None, identifier=None):
+        var_declaration_name = '{}{}'.format('VarDeclaration', identifier)
+
+        graph.node(var_declaration_name, '<f0> Name|<f1> Variable Declaration|<f2> Value')
+
+        self.__render_expression(graph, statement.initializer, '{}:f2'.format(var_declaration_name))
+
+        identifier_name = 'Identifier{}'.format(identifier)
+
+        graph.node(identifier_name, '<f0> Identifier |<f2> {}'.format(statement.name))
+        graph.edge('{}:f0'.format(var_declaration_name), '{}:f0'.format(identifier_name))
+
+        if previous_node is not None:
+            graph.edge(previous_node, '{}:f1'.format(var_declaration_name))
+
+    def render_block_statement(self, graph, identifier, previous_node, statement):
+        block_name = '{}{}'.format('Block', identifier)
+
+        graph.node(block_name, '<f0> Block | <f1> Statements')
+
+        for block_statement in statement.statements:
+            self.__render_statement(graph, block_statement, '{}:f1'.format(block_name), str(uuid.uuid4()))
+
+        if previous_node is not None:
+            graph.edge(previous_node, '{}:f1'.format(block_name))
+
+    def __render_expression(self, graph, expression, previous_node=None, identifier=None):
         if type(expression) in VALUES_TYPES:
-            expression_name = '{}{}'.format(type(expression).__name__, count)
+            expression_name = '{}{}'.format(type(expression).__name__, identifier)
 
             if type(expression) == Null:
                 graph.node(expression_name, '<f0> {}'.format(type(expression).__name__))
@@ -55,18 +84,18 @@ class GraphvizPrinter:
             if previous_node is not None:
                 graph.edge(previous_node, '{}:f0'.format(expression_name))
         elif type(expression) == Binary:
-            binary_operation_name = '{}{}'.format('BinaryOperation', count)
+            binary_operation_name = '{}{}'.format('BinaryOperation', identifier)
 
             graph.node(binary_operation_name, '<f0> Left | <f1> Binary Operation |<f2> Right')
-            graph.node('Operator{}'.format(count), '<f0> {}'.format(expression.operator))
-            graph.edge('{}:f1'.format(binary_operation_name), 'Operator{}:f0'.format(count))
+            graph.node('Operator{}'.format(identifier), '<f0> {}'.format(expression.operator))
+            graph.edge('{}:f1'.format(binary_operation_name), 'Operator{}:f0'.format(identifier))
             self.__render_expression(graph, expression.left, '{}:f0'.format(binary_operation_name), str(uuid.uuid4()))
             self.__render_expression(graph, expression.right, '{}:f2'.format(binary_operation_name), str(uuid.uuid4()))
 
             if previous_node is not None:
                 graph.edge(previous_node, '{}:f1'.format(binary_operation_name))
         elif type(expression) == Grouping:
-            grouping_name = '{}{}'.format(type(expression).__name__, count)
+            grouping_name = '{}{}'.format(type(expression).__name__, identifier)
 
             graph.node(grouping_name, '<f0> Grouping | <f1> Expression')
 
@@ -74,4 +103,3 @@ class GraphvizPrinter:
 
             if previous_node is not None:
                 graph.edge(previous_node, '{}:f0'.format(grouping_name))
-
