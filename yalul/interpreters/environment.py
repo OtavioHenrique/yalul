348382965d53bce23d8137d6f7b82da9355f16d3
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2021-03-22 21:34:41 -0300

    Improve environment

--- yalul/interpreters/environment.py
+++ yalul/interpreters/environment.py
@@ -1,3 +1,6 @@
+from yalul.parsers.ast.nodes.statements.expressions.values.null import Null
+
+
 class Environment:
     def __init__(self):
         self.environment_table = {}
@@ -6,7 +9,18 @@ class Environment:
         self.environment_table[identifier] = value
 
     def get_variable(self, identifier):
-        return self.environment_table[identifier]
+        if self.variable_exists(identifier):
+            return self.environment_table[identifier]
+        else:
+            return Null('null')
+
+    def variable_exists(self, identifier):
+        try:
+            self.environment_table[identifier]
+        except KeyError:
+            return False
+        else:
+            return True
 
     def show_environment_table(self):
         return self.environment_table
